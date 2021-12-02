38a377f9b7d9c0564b16898f43986b5a3d8256de
Bita <bita.janzadeh@hotmail.com>
2021-12-01 23:03:33 -0500

    pair programming challenge

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,36 @@
+const transpose = function(matrix) {
+    let output = [];
+    for (let col = 0; col < matrix[0].length; col++) {
+        output.push(matrix.map(x => x[col]))
+        // console.log(muse)
+    } 
+    return output;
+    };
+
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
+    // console.log(`horizontalJoin is:${horizontalJoin}`);
+
+    const verticalLetters = transpose(letters.map(ls => ls.join('')));
+    const verticalJoin = verticalLetters.map(ls => ls.join(''))
+
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        // console.log(`horizontalJoin is:${l}`);
+        if (l.includes(word)) return true;
     }
+    for (l of verticalJoin) {
+        // console.log(`horizontalJoin is:${l}`);
+        if (l.includes(word)) return true;
+    }
+    return false;
 }
 
-module.exports = wordSearch
+module.exports = wordSearch;
+    
+    
+    
+    
+    
+        
+        
+        
